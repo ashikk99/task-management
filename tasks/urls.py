e5b2d960bfeb618eac42b424d47b069fa436711d
Md.Ashik Sheikh <ashik409525sheikh@gmail.com>
2025-06-19 22:16:42 +0600

    dynamic url implemeneted

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,6 @@ from tasks.views import show_task,show_specific_task
 
 
 urlpatterns = [
-    path('show-task/', show_task)
+    path('show-task/', show_task),
     path('show-task/<int:id>',show_specific_task)
 ]
